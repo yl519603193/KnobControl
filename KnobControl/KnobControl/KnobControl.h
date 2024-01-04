7b7c1267e24b4a4d3a1bb1614d3797ee86777422
yl519603193 <519603193@qq.com>
2024-01-04 17:18:42 +0800

    测试git 仓库

---
+++ KnobControl/KnobControl/KnobControl.h
@@ -0,0 +1,18 @@
+//
+//  KnobControl.h
+//  KnobControl
+//
+//  Created by apple mini on 2024/1/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KnobControl.
+FOUNDATION_EXPORT double KnobControlVersionNumber;
+
+//! Project version string for KnobControl.
+FOUNDATION_EXPORT const unsigned char KnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl/PublicHeader.h>
+
+
